55cdf31076246933c7c81f544d763ab14a62bb9a
fasya mazaya <fasyamazaya@gmail.com>
2023-05-19 09:32:47 +0700

    updated

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,9 @@ Route::get('/home', function () {
     if (session('status')) {
         return redirect()->route('home')->with('status', session('status'));
     }
-
     return redirect()->route('home');
 });
+
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('place/{place}', 'HomeController@show')->name('place');
 
@@ -19,6 +19,7 @@ Auth::routes();
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'middleware' => ['auth']], function () {
     Route::get('/', 'HomeController@index')->name('home');
+
     // Permissions
     Route::delete('permissions/destroy', 'PermissionsController@massDestroy')->name('permissions.massDestroy');
     Route::resource('permissions', 'PermissionsController');
